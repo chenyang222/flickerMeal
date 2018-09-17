d0c26f75dd15c52fd6888fcb68286c1a19daaf44
ranqiyang11 <ranqiyang@jiayunhui.com>
2018-09-17 18:50:46 +0800

    09/17

--- pages/index/shouye/shouye.js
+++ pages/index/shouye/shouye.js
@@ -1,15 +1,18 @@
 //index.js
 //获取应用实例
 const app = getApp()
-var utils = require('../../../utils/util.js');
 var bmap = require('../../../utils/bmap-wx.min.js'); // 引入百度地图js
 var wxMarkerData = []; //定位成功回调对象
+var cityCodeJson = require('../../../utils/cityCode.js'); // 引入cityJson.js
+var utils = require('../../../utils/util.js');
 Page({
   data: {
     imgdata: app.globalData.imgdata,
-
-    ak: 'NPfvQSlaxLvtuBWm4YDVwecQNoTACuUY', // 填写申请到的ak
     currentCity: '',
+    cityCodeJson: [], // city.js
+    machineList: [], // 机器列表
+    machineAllList: [],
+    isShouAllMachine: false,
 
     autoplay: true,
     imgsDotBanner: [], // 轮播banner
@@ -75,6 +78,12 @@ Page({
     })
     // 获取优惠券
     this.getCoupon();
+    // city.js 引入data
+    this.setData({
+      cityCodeJson: cityCodeJson.city
+    })
+    // 定位并获取机器列表
+    this.setPosition();
     // 菜单
     this.getMenuClass();
     // 左侧餐品分类导航
@@ -101,28 +110,6 @@ Page({
     //   })
     // 获取机器评价
     this.getMachineEva();
-  },
-  // 定位设置
-  setPosition: function () {
-    var that = this;
-    // 新建百度地图对象 
-    var BMap = new bmap.BMapWX({
-      ak: that.data.ak
-    });
-    var fail = function (data) {
-      console.log(data)
-    };
-    var success = function (data) {
-      const posData = data.originalData.result;
-      that.setData({
-        currentCity: posData.formatted_address
-      });
-    }
-    // 发起POI检索请求 
-    BMap.regeocoding({
-      fail: fail,
-      success: success
-    });
   },
   //点击定位跳转
   posiIn: function () {
@@ -577,5 +564,98 @@ Page({
   plus: function (e) {
     let num = e.currentTarget.dataset.buynumber + 1;
     this.editShoppingNum(e.currentTarget.dataset.productid, num)
+  },
+  // 定位设置
+  setPosition: function () {
+    var that = this;
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({
+      ak: app.globalData.ak
+    });
+    var fail = function (data) {
+      app.fetch({
+        url: '/fastfood/foodmachine/findByAreaCode'
+      })
+        .then((response) => {
+          // 获取推荐机器人
+          that.setData({
+            machineList: response
+          })
+        })
+    };
+    var success = function (data) {
+      const posData = data.originalData.result;
+      that.setData({
+        currentCity: posData.formatted_address
+      })
+      var cityStrArr = [];
+      cityStrArr.push(posData.addressComponent.province);
+      cityStrArr.push(posData.addressComponent.city);
+      cityStrArr.push(posData.addressComponent.district);
+      that.getMachineByCityCode(cityStrArr);
+    }
+    // 发起POI检索请求 
+    BMap.regeocoding({
+      fail: fail,
+      success: success
+    });
+  },
+  // 根据code获取机器
+  getMachineByCityCode: function (cityStrArr) {
+    var that = this;
+    var cityCodeJson = this.data.cityCodeJson;
+    var areaCode = [];
+    for (let i = 0; i < cityCodeJson.length; i++) {
+      if (cityStrArr[0] == cityCodeJson[i].name) {
+        areaCode.push(i);
+        if (cityStrArr[0] == '北京市') {
+          for (let j = 0; j < cityCodeJson[i].sub.length; j++) {
+            if (cityStrArr[2] == cityCodeJson[i].sub[j].name) {
+              areaCode.push(j);
+            }
+          }
+          areaCode.push(0);
+        } else {
+          for (let j = 0; j < cityCodeJson[i].sub.length; j++) {
+            if (cityStrArr[1] == cityCodeJson[i].sub[j].name) {
+              areaCode.push(j);
+              for (let a = 0; a < cityCodeJson[i].sub[j].sub.length; a++) {
+                if (cityStrArr[2] == cityCodeJson[i].sub[j].sub[a].name) {
+                  areaCode.push(a);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    app.fetch({
+      url: '/fastfood/foodmachine/findByAreaCode',
+      method: 'get',
+      data: {
+        // areaCode: areaCode.join(',')
+        areaCode: '0,4,0'
+      }
+    })
+      .then((response) => {
+        // 获取推荐机器人
+        that.setData({
+          machineList: response.slice(0,1),
+          machineAllList: response
+        })
+      })
+  },
+  // 设置机器id
+  setStorgeId: function (e) {
+    const machineId = e.currentTarget.dataset.machineid;
+    const machineName = e.currentTarget.dataset.machinename;
+    wx.setStorageSync('machineId', machineId)
+    wx.setStorageSync('machineName', machineName)
+    this.onLoad();
+  },
+  showAllMachine: function () {
+    this.setData({
+      isShouAllMachine: !this.data.isShouAllMachine
+    })
   }
 })
